90677eef75da616386b9cb1e82e4a1bfc0d2d968
koraygnsl <93931519+koraygnsl@users.noreply.github.com>
2022-10-10 19:13:50 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+import cv2
+import cvzone
+
+thres = 0.55
+nmsThres = 0.2
+cap = cv2.VideoCapture(0)
+cap.set(3, 1920)
+cap.set(4, 1080)
+
+classNames = []
+classFile = 'coco.names'
+with open(classFile, 'rt') as f:
+    classNames = f.read().split('\n')
+print(classNames)
+configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+weightsPath = "frozen_inference_graph.pb"
+
+net = cv2.dnn_DetectionModel(weightsPath, configPath)
+net.setInputSize(320, 320)
+net.setInputScale(1.0 / 127.5)
+net.setInputMean((127.5, 127.5, 127.5))
+net.setInputSwapRB(True)
+
+
+while True:
+    success, img = cap.read()
+    img = cv2.flip(img, 1)
+    classIds, confs, bbox = net.detect(img, confThreshold=thres, nmsThreshold=nmsThres)
+
+    try:
+        for classId, conf, box in zip(classIds.flatten(), confs.flatten(), bbox):
+            cvzone.cornerRect(img, box)
+            cv2.putText(img, f'{classNames[classId - 1].upper()}',
+                        (box[0] + 10, box[1] + 30), cv2.FONT_HERSHEY_COMPLEX_SMALL,
+                        1, (0, 255, 0), 2)
+    except:
+        pass
+
+    cv2.namedWindow("Image", cv2.WND_PROP_FULLSCREEN)
+    cv2.setWindowProperty("Image", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+    cv2.imshow("Image", img)
+    if cv2.waitKey(1) == ord('q'):
+        break
